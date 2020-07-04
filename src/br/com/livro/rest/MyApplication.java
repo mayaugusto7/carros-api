4bbf563fd42686282fefb1721187c255437951af
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2020-07-04 17:34:32 -0300

    Finalizando projeto de api de carros

---
+++ src/br/com/livro/rest/MyApplication.java
@@ -0,0 +1,28 @@
+package br.com.livro.rest;
+
+import org.glassfish.jersey.jettison.JettisonFeature;
+
+import javax.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MyApplication extends Application {
+
+    // Não é mais necessario por conta da implementação das intefaces MessageBodyWriter<Object>, MessageBodyReader<Object>
+    // para utilizar o Gson
+    //@Override
+    //public Set<Object> getSingletons() {
+    //    Set<Object> singletons = new HashSet<>();
+    //    singletons.add(new JettisonFeature());
+    //    return singletons;
+    //}
+
+    @Override
+    public Map<String, Object> getProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("jersey.config.server.provider.packages", "br.com.livro");
+        return properties;
+    }
+}
